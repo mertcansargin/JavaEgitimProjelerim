b1b9cb915720503e29885ad81636b19a0a956cd8
Mert <77789139+mertcansargin@users.noreply.github.com>
2022-08-10 18:06:51 +0300

    Add files via upload

---
+++ PalindromSayiBulanProgram/src/Main.java
@@ -0,0 +1,19 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Main {
+    static boolean isPalindrom(int number){
+        int temp=number, reverseNumber=0,lastnumber;
+        while(temp!=0){
+            lastnumber=temp%10;
+            reverseNumber=(reverseNumber*10)+lastnumber;
+            temp/=10;
+        }
+        if(number==reverseNumber)
+        return true;
+        else
+            return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(145625555));
+    }
+}
