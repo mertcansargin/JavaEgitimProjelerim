cdae8d435ffe67123fc59d81749cf9ee9794e382
Mert <77789139+mertcansargin@users.noreply.github.com>
2022-11-27 18:46:49 +0300

    Add files via upload

---
+++ StudentNoteSystem/src/Course.java
@@ -0,0 +1,25 @@
+public class Course {
+    Teacher teacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+    public Course(String name, String code, String prefix) {
+        this.name=name;
+        this.code=code;
+        this.prefix=prefix;
+        this.note=0;
+    }
+    public void addTeacher(Teacher teacher){
+        if (teacher.branch.equals(this.name)){
+            this.teacher=teacher;
+            printTeacher();
+        }
+        else {
+            System.out.println("Girilen dersler hoca ile uyuşmuyor...");
+        }
+    }
+    void printTeacher(){
+        this.teacher.print();
+    }
+}
